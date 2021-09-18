3d898bfd9d8117c1af28dd862d4c75b0d946f05e
Wang, Vincent H <vincent.wang19@imperial.ac.uk>
2021-09-18 20:40:42 +0100

    Adds Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import './App.css';
+import Navbar from './components/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+
+          <h1>
+            Learn React
+          </h1>
+
+          <Route path="/about" exact>
+            <h1>About</h1>{/* About */}
+          </Route>
+
+          <Route path="/home" exact>
+            <h1>Home</h1>{/* Home */}
+          </Route>
+          
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
