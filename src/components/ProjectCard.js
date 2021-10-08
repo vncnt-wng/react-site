b95f128729f0063577ddf6ea43983a53e5c03786
Wang, Vincent H <vincent.wang19@imperial.ac.uk>
2021-10-08 02:31:54 +0100

    Makes api call to contentful to get projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,18 +5,30 @@ import Card from "@material-ui/core/Card"
 import Typography from "@material-ui/core/Typography"
 
 
-class ProjectCard extends Component {
-  render() {
-    return (
-      <Grid item xs={12} lg={6}>
-        <Card>
-            <Typography>1</Typography>
-        </Card>
-      </Grid>
-    )
-  }
+const ProjectCards = ({projects}) => {
+  // Sort the project by their order date
+  projects.sort(function(a, b){
+    return new Date(b.fields.orderDate) - new Date(a.fields.orderDate)
+  });
+  console.log(projects)
+  return (
+    <Grid container spacing={2}>
+      {projects.map((project, index) => <ProjectCard key={index} project={project}/>)}
+    </Grid>
+  )
 }
 
-export default ProjectCard
 
+const ProjectCard = ({project}) => {
+  const {title} = project.fields  
+  const id = project.sys.id
+  return (
+    <Grid item xs={12} lg={6}>
+      <Card>
+          <Typography>{title}: {id}</Typography>
+      </Card>
+    </Grid>
+  )
+}
 
+export default ProjectCards
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,41 +3,34 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Grid from "@material-ui/core/Grid"
 import Paper from "@material-ui/core/Paper"
 import Typography from "@material-ui/core/Typography"
-import ProjectCard from "./ProjectCard";
+import ProjectCards from "./ProjectCard";
+import { client } from "../ContentfulClient";
 
 
 class Projects extends Component {
+  state = {
+    projects: []
+  }
+
+  // Make the contenful api call
+  componentDidMount() {
+    client.getEntries({
+      content_type: "project"
+    })
+    .then((response) => {
+      console.log(response)
+      this.setState({
+        projects: response.items
+      })
+    })
+    .catch(console.error)
+  }
+
   render() {
     return (
       <div>
           <h2>Here are some projects I've done...</h2>
-          <Grid container spacing={2}>
-            <Grid item xs={12} lg={6}>
-              <Paper>
-                <Typography>1</Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-              <Paper>
-                <Typography>1</Typography>
-              </Paper>
-            </Grid>
-
-            <Grid item xs={12} lg={6}>
-              <Paper>
-                <Typography>1</Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-              <Paper>
-                <Typography>1</Typography>
-              </Paper>
-            </Grid>
-
-
-            <ProjectCard />
-          </Grid>
-
+          <ProjectCards projects={this.state.projects}/>
       </div>
     )
   }
